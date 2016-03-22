91ea68ec46dae737382f2e0a659fd2340290e7e2
blopez46 <Student@et.qcc.edu>
2016-03-22 14:03:00 -0400

    Section 8 Complete

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 class ArticlesController < ApplicationController
 
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  # snipped for brevity
+
+
+
   def new
     @article = Article.new
   end
